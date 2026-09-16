d00fca82a9cf6ea4c547522eba75bda8a1b6f5cc
Jazeel Zainudeen <zainudheenjazeel@gmail.com>
2026-09-17 02:35:30 +0530

    feat: integrate Tawk.to live chat widget and build secrets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Manrope, Sora } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -179,6 +180,21 @@ export default function RootLayout({
         {children}
         {process.env.NEXT_PUBLIC_GA_ID && (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
+        {process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID && (
+          <Script id="tawk-to" strategy="lazyOnload">
+            {`
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/${process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID}/${process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID || 'default'}';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `}
+          </Script>
         )}
       </body>
     </html>
