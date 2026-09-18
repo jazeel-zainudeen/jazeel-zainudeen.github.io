3874e67aa6c92f6a7f9d24fe25668a19a0b5b60d
Jazeel Zainudeen <zainudheenjazeel@gmail.com>
2026-09-18 22:28:46 +0530

    style: increase profile portrait dimensions to 768x960

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -717,8 +717,8 @@ export default function Home() {
                   <Image 
                     src="/assets/seo/profile-portrait.webp" 
                     alt="Jazeel Zainudeen, Full Stack Developer" 
-                    width={600}
-                    height={750}
+                    width={768}
+                    height={960}
                     sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 400px"
                     className="relative aspect-[4/5] w-full object-cover object-top"
                   />
