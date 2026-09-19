82d4b4a23d9aba1669900db08de319428f08b389
Jazeel Zainudeen <zainudheenjazeel@gmail.com>
2026-09-20 00:27:19 +0530

    chore: update browserslist versions and profile portrait image dimensions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -717,8 +717,8 @@ export default function Home() {
                   <Image 
                     src="/assets/seo/profile-portrait.webp" 
                     alt="Jazeel Zainudeen, Full Stack Developer" 
-                    width={768}
-                    height={960}
+                    width={500}
+                    height={625}
                     sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 400px"
                     className="relative aspect-[4/5] w-full object-cover object-top"
                   />
